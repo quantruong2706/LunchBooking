f10c62f5f3c96e297d44d0dd7332ef9f48978eb3
LDAP\ntdat5 <ntdat5.cmc@gmail.com>
2022-12-26 16:30:39 +0700

    temp commit

--- src/router/App.tsx
+++ src/router/App.tsx
@@ -2,8 +2,19 @@ import { RouterProvider } from 'react-router-dom'
 import { store } from '../stores'
 import { Provider } from 'react-redux'
 import router from '@app/router/Router'
-
+import { usersCol } from '../server/useDB'
+import { doc, getDoc } from 'firebase/firestore'
+import { useEffect } from 'react'
 function App() {
+  const getFbDoc = async () => {
+    const userDocRef = doc(usersCol, 'dtqhuong1')
+    const userDoc = await getDoc(userDocRef)
+    const userData = userDoc.data()
+    if (userData) console.log('asdasdasd', userData)
+  }
+  useEffect(() => {
+    getFbDoc()
+  }, [])
   return (
     <Provider store={store}>
       <RouterProvider router={router} />
