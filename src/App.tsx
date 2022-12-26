68ac7e176160dbba151a9b8cc3c00b6a833358b0
dat09 <ltdat8@cmcglobal.vn>
2022-12-26 16:25:03 +0700

    add login, config route

---
+++ src/App.tsx
@@ -0,0 +1,55 @@
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
+import { useEffect } from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { RouterProvider } from 'react-router-dom'
+
+import Router from './router/Router'
+import { auth, db } from './server/firebase'
+import { useAppDispatch } from './stores/hook'
+import { setUser } from './stores/user'
+
+function App() {
+  const [loggedInUser, loading] = useAuthState(auth)
+  const dispatch = useAppDispatch()
+  useEffect(() => {
+    const setUserInDb = async () => {
+      try {
+        await setDoc(
+          doc(db, 'Users', loggedInUser?.email as string),
+          {
+            email: loggedInUser?.email,
+            lastSeen: serverTimestamp(),
+            uid: loggedInUser?.uid,
+            // photoURL: loggedInUser?.photoURL,
+          },
+          { merge: true }
+        )
+      } catch (error) {
+        console.log('ERROR SETTING USER INFO IN DB', error)
+      }
+    }
+
+    if (loggedInUser) {
+      const { uid, displayName, email, photoURL } = loggedInUser
+      dispatch(
+        setUser({
+          uid,
+          displayName: displayName || 'unknown',
+          email: email || 'undefined',
+          photoURL: photoURL || '',
+        })
+      )
+      setUserInDb()
+    }
+  }, [loggedInUser, dispatch])
+  if (loading) {
+    return (
+      <div>
+        <span>Loading...</span>
+      </div>
+    )
+  }
+  return <RouterProvider router={Router} />
+}
+
+export default App
