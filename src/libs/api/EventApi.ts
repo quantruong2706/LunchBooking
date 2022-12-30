a2d9fb1f75d6e26a29f87698fd31f05a1076bb44
datnt01 <datntse03522@gmail.com>
2022-12-30 11:28:54 +0700

    Feat/lb13 create lunch (#15)
    * add genarate user to pay
    
    * feat/LB13: update generate bill owner, round number when share bill
    
    * feat/LB13: detail bill
    
    * add event
    
    * fix ui
    
    Co-authored-by: LDAP\ntdat5 <ntdat5.cmc@gmail.com>
    Co-authored-by: Dao Huong Milo <dao.huong@thoughtworks.com>

--- src/libs/api/EventApi.ts
+++ src/libs/api/EventApi.ts
@@ -1,4 +1,4 @@
-import { Event, User } from '@app/server/firebaseType'
+import { IEvent, User } from '@app/server/firebaseType'
 import { EventColection, UserDetail, usersColection } from '@app/server/useDB'
 import { doc, getDocs, setDoc, updateDoc } from 'firebase/firestore'
 
@@ -11,8 +11,8 @@ export const getListUser = async () => {
   })
   return listUser
 }
-export const setEvent = async (data: Event) => {
-  const userRef = doc(EventColection, 'user_12345')
+export const setEvent = async (data: IEvent) => {
+  const userRef = doc(EventColection)
   let isSuccess = false
   await setDoc(userRef, data).then(() => {
     isSuccess = true
