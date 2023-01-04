3508f3064ebcd58b4c1a9ca1be41ebef909fa4a5
datnt01 <datntse03522@gmail.com>
2023-01-04 14:52:53 +0700

    Lb 25 update fix (#20)
    * change structure of event on firebase  when create event
    
    * change fire store congif
    
    Co-authored-by: LDAP\ntdat5 <ntdat5.cmc@gmail.com>

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -6,12 +6,12 @@ const { VITE_BASE_API_KEY } = import.meta.env
 
 const firebaseConfig = {
   apiKey: VITE_BASE_API_KEY,
-  authDomain: 'an-lunch.firebaseapp.com',
-  projectId: 'an-lunch',
-  storageBucket: 'an-lunch.appspot.com',
-  messagingSenderId: '593640378675',
-  appId: '1:593640378675:web:aef77080d743c6ccfb2e46',
-  measurementId: 'G-FD0279B6RW',
+  authDomain: 'lunchbooking-d4fbf.firebaseapp.com',
+  projectId: 'lunchbooking-d4fbf',
+  storageBucket: 'lunchbooking-d4fbf.appspot.com',
+  messagingSenderId: '123054878462',
+  appId: '1:123054878462:web:7673277f92786f742abfaf',
+  measurementId: 'G-Q9MMWCJ82S',
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
