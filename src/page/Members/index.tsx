2a41b81ded7a2f64bf933b8760f01528bcf53652
Phúc (Scott) Nguyễn <en.deathraven@gmail.com>
2023-01-05 08:53:25 +0700

    LB18: feat - add search component and function

--- src/page/Members/index.tsx
+++ src/page/Members/index.tsx
@@ -2,6 +2,7 @@ import { getListUser } from '@app/libs/api/EventApi'
 import React, { useEffect, useState } from 'react'
 import { User } from '@app/server/firebaseType'
 import ProfilePicture from '@app/assets/profile-picture.png'
+import Search from '@app/page/Members/Search'
 
 const Members = () => {
   const [users, setUsers] = useState<User[]>([])
@@ -12,8 +13,16 @@ const Members = () => {
     })
   }, [])
 
+  const handleSubmit = (query: string) => {
+    console.log(query)
+    const filteredUserList = users.filter((user) => {
+      return user.name?.toLowerCase().includes(query)
+    })
+    setUsers(filteredUserList)
+  }
+
   const membersList = users.map((user) => (
-    <div className="bg-white px-1 py-3 rounded-xl flex items-center gap-2">
+    <div className="bg-white px-1 py-3 rounded-xl flex items-center gap-2" key={user.uid}>
       <img src={ProfilePicture} alt="User profile" className="w-14 h-14 rounded-full shadow-lg" />
       <div className="flex flex-col pr-1">
         <p>{user.name}</p>
@@ -27,8 +36,11 @@ const Members = () => {
   return (
     <div className="flex flex-col items-center pt-6 pb-12">
       <h1 className="font-bellota text-center text-3xl pb-4">Thành viên</h1>
-      <input type="text" className="rounded-full max-w-xs px-6 py-2.5 min-w-[300px] md:min-w-[600px]" placeholder="Search" />
-      <div className="pt-6 flex flex-col gap-4">{membersList}</div>
+      <Search onSubmit={handleSubmit} />
+      <div className="pt-6 flex flex-col gap-4">
+        {users.length === 0 && <div>Team Front-end không có ai cả!</div>}
+        {membersList}
+      </div>
     </div>
   )
 }
