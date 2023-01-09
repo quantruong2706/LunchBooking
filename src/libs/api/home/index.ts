494c23b9b99d49368eafeef2046de1dfea95fe40
Duy. Bui Quang - CMC Global DE1 <bqduy1@cmcglobal.vn>
2023-01-09 15:04:06 +0700

    feat/LB19 update dashboard home api

--- src/libs/api/home/index.ts
+++ src/libs/api/home/index.ts
@@ -2,22 +2,43 @@ import { EventColection, EventDetailColection } from '@app/server/useDB'
 import { store } from '@app/stores'
 import { CollectionReference, getDocs, query, where } from 'firebase/firestore'
 
-const getBy = async <T = any>(cloection: CollectionReference<T>, params: string) => {
-  return (await getDocs(query(cloection, where(params, '==', store.getState().USER.data.uid)))).docs.map((item) => ({ ...item.data(), id: item.id }))
+const getBy = async <T = any>(cloection: CollectionReference<T>, params?: string) => {
+  return (await getDocs(params ? query(cloection, where(params, '==', store.getState().USER.data.uid)) : cloection)).docs.map((item) => ({
+    ...item.data(),
+    id: item.id,
+  }))
 }
 export const getHomeData = async () => {
-  //số bữa ăn user chủ chi
-  const isHost = await getBy(EventColection, 'userPayId')
+  const uid = store.getState().USER.data.uid
+  //list bữa ăn user chủ chi
+  const allEvent = await getBy(EventColection)
+  const isHost = allEvent.filter((item) => item.userPayId === uid)
+
   // số bữa ăn user tham gia
-  const isMember = await getBy(EventDetailColection, 'uid')
+  const allEventDefail = await getBy(EventDetailColection)
+  const isMember = allEventDefail.filter((item) => item.uid === uid)
+
   // list bữa ăn user chưa trả
-  const unPaidList = isMember.filter((item) => !item.isPaid)
+  const unPaidList = isMember
+    .filter((item) => !item.isPaid)
+    .map((item) => ({ ...item, eventName: allEvent.find((event) => event.id === item.eventId)?.eventName }))
+
   // list bữa ăn user chưa đòi
-  const requirePaymentList = isHost.filter((item) => !item.isAllPaid)
+  const requirePaymentList = isHost
+    .filter((item) => !item.isAllPaid)
+    .map((item) => {
+      const paid = allEventDefail.filter((event) => event.eventId === item.id && event.isPaid).reduce((sum, event) => sum + Number(event.amount!), 0)
+      return {
+        ...item,
+        totalAmount: item.totalAmount! - paid,
+      }
+    })
+
+  const isMemberCount = isMember.length - isHost.length
 
   return {
-    isHost,
-    isMember,
+    isHostCount: isHost.length,
+    isMemberCount: isMemberCount > 0 ? isMemberCount : 0,
     unPaidList,
     requirePaymentList,
   }
--- src/page/Home/index.tsx
+++ src/page/Home/index.tsx
@@ -1,10 +1,9 @@
 import { PAGES } from '@app/contants'
 import { getHomeData } from '@app/libs/api/home'
-import { setSelectedListMember } from '@app/stores/events'
+import { IEvent, IEventDetail } from '@app/server/firebaseType'
 import { setCurrentPage } from '@app/stores/footer'
 import { useAppSelector } from '@app/stores/hook'
 import { userStore } from '@app/stores/user'
-import { list } from '@firebase/storage'
 import { Grid } from '@mui/material'
 import { Container } from '@mui/system'
 import { useEffect, useState } from 'react'
@@ -20,15 +19,16 @@ export default function HomePage() {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(setCurrentPage(PAGES.HOME));
+    dispatch(setCurrentPage(PAGES.HOME))
 
     getHomeData().then((e) => {
       setListEvent(e)
+      // console.log("aaa", e);
     })
   }, [])
 
   const [listEvent, setListEvent] = useState<any>()
-  console.log('listEvent',listEvent);
+  // console.log('listEvent', listEvent)
   const css = `
     html {
       width: 100vw;
@@ -107,15 +107,18 @@ export default function HomePage() {
   `
 
   const getTotalUnPaidAmount = () => {
-    return 0;
+    // let result = 0;
+    const result = listEvent.unPaidList.reduce((total: number, data: IEventDetail) => (total += data.amount || 0), 0)
+    return Math.round(result)
   }
 
   const getTotalRequirePayment = () => {
-    return 0;
+    // let result = 0;
+    const result = listEvent.requirePaymentList.reduce((total: number, data: IEvent) => (total += data.totalAmount || 0), 0)
+    return Math.round(result)
   }
 
-  return (
-    (listEvent ? 
+  return listEvent ? (
     <Container>
       <style>{css}</style>
       <Grid id="header" container direction="row" alignItems="center">
@@ -125,71 +128,81 @@ export default function HomePage() {
         </Grid>
         <Grid item xs={2}>
           <Link to="/profile">
-            <img id="userImg" src={user.photoURL} alt="user_photo" referrerPolicy="no-referrer" />
+            <img id="userImg" src={user.photoURL!} alt="user_photo" referrerPolicy="no-referrer" />
           </Link>
         </Grid>
       </Grid>
       <Grid id="dashboard" container direction="row" alignItems="center" justifyContent="center" spacing={3} sx={{ marginLeft: { xs: '-12px', sm: '0' } }}>
         <Grid className="item box" item xs={6} sm={3} sx={{ maxWidth: { xs: '43vw', sm: '20vw', lg: '14vw' } }}>
           <p className="itemHeader">Tham gia</p>
-          <p className="itemDetail">{listEvent.isMember.length}</p>
+          <p className="itemDetail">{listEvent.isMemberCount} lần</p>
         </Grid>
         <Grid className="item box" item xs={6} sm={3} sx={{ maxWidth: { xs: '43vw', sm: '20vw', lg: '14vw' } }}>
           <p className="itemHeader">Chủ chi</p>
-          <p className="itemDetail">{listEvent.isHost.length}</p>
+          <p className="itemDetail">{listEvent.isHostCount} lần</p>
         </Grid>
         <Grid className="item box" item sm={3} sx={{ display: { xs: 'none', sm: 'block' }, maxWidth: { sm: '20vw', lg: '14vw' } }}>
           <p className="itemHeader">Cần trả</p>
-          <p className="itemDetail">{listEvent.unPaidList.length}</p>
+          <p className="itemDetail">{getTotalUnPaidAmount()}K</p>
         </Grid>
         <Grid className="item box" item sm={3} sx={{ display: { xs: 'none', sm: 'block' }, maxWidth: { sm: '20vw', lg: '14vw' } }}>
           <p className="itemHeader">Cần đòi</p>
-          <p className="itemDetail">{listEvent.requirePaymentList.length}</p>
+          <p className="itemDetail">{getTotalRequirePayment()}K</p>
         </Grid>
-        </Grid>
-        <Grid id="list" container direction="row" justifyContent="center" spacing={3} sx={{marginLeft: { xs: '-12px', sm: '0'} }}>
-          <Grid className="item box" item xs={12} sm={6} sx={{maxWidth: { sm: '43vw', lg: '29vw'} }}>
-            <div>
-              <span className="text-bold">Số bữa chưa trả</span>
-              <span className="text-right">{listEvent.unPaidList.length} bữa</span>
-            </div>
-            <hr className="divider"/>
-            {listEvent.unPaidList.length > 0 ? listEvent.unPaidList.map(data =>
+      </Grid>
+      <Grid id="list" container direction="row" justifyContent="center" spacing={3} sx={{ marginLeft: { xs: '-12px', sm: '0' } }}>
+        <Grid className="item box" item xs={12} sm={6} sx={{ maxWidth: { sm: '43vw', lg: '29vw' } }}>
+          <div>
+            <span className="text-bold">Số bữa chưa trả</span>
+            <span className="text-right">{listEvent.unPaidList.length} bữa</span>
+          </div>
+          <hr className="divider" />
+          {listEvent.unPaidList.length > 0 ? (
+            listEvent.unPaidList.map((data: any) => (
               <div key={data.id}>
-                <Link to={'/events/' + data.eventId} className="text-link">{data.name}</Link>
-                <span className="text-right">{data.amount}</span>
+                <Link to={'/events/' + data.eventId} className="text-link">
+                  {data.eventName}
+                </Link>
+                <span className="text-right">{data.amount}K VND</span>
               </div>
-             ): <img src="/src/assets/paid_logo.webp" alt="paid"/>}
-            <hr className="divider"/>
-            <div>
-              <span className="text-bold">Tổng</span>
-              <span className="text-right">{getTotalUnPaidAmount()}</span>
-            </div>
-          </Grid>
-          <Grid className="item box" item xs={12} sm={6} sx={{maxWidth: { sm: '43vw', lg: '29vw'} }}>
+            ))
+          ) : (
+            <img src="/src/assets/paid_logo.webp" alt="paid" />
+          )}
+          <hr className="divider" />
+          <div>
+            <span className="text-bold">Tổng</span>
+            <span className="text-right">{getTotalUnPaidAmount()}K VND</span>
+          </div>
+        </Grid>
+        <Grid className="item box" item xs={12} sm={6} sx={{ maxWidth: { sm: '43vw', lg: '29vw' } }}>
           <div>
             <span className="text-bold">Số bữa chưa đòi</span>
             <span className="text-right">{listEvent.requirePaymentList.length} bữa</span>
           </div>
           <hr className="divider" />
           {listEvent.requirePaymentList.length > 0 ? (
-            listEvent.requirePaymentList.map((data) => (
+            listEvent.requirePaymentList.map((data: any) => (
               <div key={data.id}>
                 <Link to={'/events/' + data.id} className="text-link">
                   {data.eventName}
                 </Link>
-                <span className="text-right">{data.members ? Math.round(data.totalAmount || 0 / data.members.length) : 0}</span>
+                <span className="text-right">{Math.round(data.totalAmount)}K VND</span>
               </div>
-             ))): <img src="/src/assets/paid_logo.webp" alt="paid"/>}
-            <hr className="divider"/>
-            <div>
-              <span className="text-bold">Tổng</span>
-              <span className="text-right">{getTotalRequirePayment()}</span>
-            </div>
-          </Grid>
+            ))
+          ) : (
+            <img src="/src/assets/paid_logo.webp" alt="paid" />
+          )}
+          <hr className="divider" />
+          <div>
+            <span className="text-bold">Tổng</span>
+            <span className="text-right">{getTotalRequirePayment()}K VND</span>
+          </div>
+        </Grid>
       </Grid>
       <div></div>
     </Container>
-    : <></>)
+  ) : (
+    <></>
   )
 }
