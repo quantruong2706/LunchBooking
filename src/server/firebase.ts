dded1b2977c866918bff501fee4d2254539d477a
dat09 <ltdat8@cmcglobal.vn>
2023-01-10 11:47:13 +0700

    feat/define-env: add env content

--- src/server/firebase.ts
+++ src/server/firebase.ts
@@ -3,24 +3,24 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { initializeFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-const { VITE_BASE_API_KEY } = import.meta.env
+const {
+  VITE_APP_apiKey: apiKey,
+  VITE_APP_authDomain: authDomain,
+  VITE_APP_projectId: projectId,
+  VITE_APP_storageBucket: storageBucket,
+  VITE_APP_messagingSenderId: messagingSenderId,
+  VITE_APP_appId: appId,
+  VITE_APP_measurementId: measurementId,
+} = import.meta.env
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDBiz4TUFtdQTE9ISHK5S5dtP6GIUaF8AU',
-  authDomain: 'lunchbooking-d4fbf.firebaseapp.com',
-  projectId: 'lunchbooking-d4fbf',
-  storageBucket: 'lunchbooking-d4fbf.appspot.com',
-  messagingSenderId: '123054878462',
-  appId: '1:123054878462:web:7673277f92786f742abfaf',
-  measurementId: 'G-Q9MMWCJ82S',
-  // apiKey: 'AIzaSyDcsGPXU5iaQXyr5fxxft0q9Bvf8wcNu9Q',
-  // authDomain: 'an-lunch.firebaseapp.com',
-  // databaseURL: 'https://an-lunch-default-rtdb.asia-southeast1.firebasedatabase.app',
-  // projectId: 'an-lunch',
-  // storageBucket: 'an-lunch.appspot.com',
-  // messagingSenderId: '593640378675',
-  // appId: '1:593640378675:web:aef77080d743c6ccfb2e46',
-  // measurementId: 'G-FD0279B6RW',
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
 }
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig)
